a92fef3fa4a11dd6cf7f8bb6f66ced4c242e07c5
David Pineda <david@deparadise.com>
2016-02-29 23:07:45 -0800

    added livereload to the sass workflow for gulp

--- gulpfile.js
+++ gulpfile.js
@@ -4,9 +4,12 @@
 		//concat = require('gulp-concat'),
 		//gulpIf = require('gulp-if'),
 		//jsHint = require('gulp-jshint'),
-		//livereload = require('gulp-livereload'),
+		livereload = require('gulp-livereload'),
 		//uglify = require('gulp-uglify');
 		gutil = require('gulp-util');
+
+/// ASSET SOURCE LIST
+	var sassSources = ['-assets/sass/**/*.scss'];
 
 // compass >>> Styles pipe
 	gulp.task('compass', function(){
@@ -19,15 +22,24 @@
 			style: 'expanded',  // expanded for dev / compressed for prod
 			image: '-assets/imgs'
 		})
-		.on('error', gutil.log));
+		.on('error', gutil.log))
 		//.pipe(gulp.dest('/style.css'))
-		//.pipe(livereload())
+		.pipe(livereload());
+	});
+
+/// GULP RUNNING FUNCTIONS
+// watch >>> Watches files for changes and runs workflow pipes
+	gulp.task('watch', function(){
+		livereload.listen();
+		//gulp.watch(staticSources, ['static']);
+		gulp.watch(sassSources, ['compass']);
+		//gulp.watch(jsSources, ['js']);
 	});
 
 /// DEFAULT >>> Run all dev tasks and init 'watch'
 	gulp.task('default', [
 		//'static', 
-		'compass'
+		'compass',
 		//'js', 
-		//'watch'
+		'watch'
 	]);
