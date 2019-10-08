2598157fa7fcd364877a758774c0af5ee11f3a30
David Pineda <david.pineda@deparadise.com>
2019-10-07 21:29:56 -0700

    Return gallery_reel config to a single instant target configuration

--- plugins/jQuery/MixItUp_to_webflow_gallery_reel/webflow.galleryReel.config.js
+++ plugins/jQuery/MixItUp_to_webflow_gallery_reel/webflow.galleryReel.config.js
@@ -5,122 +5,71 @@
 // On document ready:
 $(document).ready(function() {
 	$(async function() {
-		let galleryReels = await $('.gallery-reel');
+		let galleryReel = await $('#category-artwork-gallery');
 		let testFilters = await $('.test-filter');
 
 		// clean up .test-filters
 		testFilters.remove();
 
-		// >>> Find all .gallery-reel and Loop
-		let mixedUpGalleryReels = await galleryReels.each(async(i, gallery) => {
-			console.log('>> GALLERY DOM:', gallery);
+		console.log('>> GALLERY DOM:', galleryReel);
+		// Collect GR_Id #category-<catKey>
+		let galleryId = galleryReel.attr('id');
+		// and split destructure for second key
+		let [type, categoryKey] = galleryId.split('-');
+		// find .gallery-filter-menu node
+		let filterMenu = galleryReel.find('.gallery-filter-menu');
+		let galleryItems = galleryReel.find('.sort-collection').children('.mix');
+		
+		// define fillter_collection
+		let galleryCollectedSubCategories = ['all'];
+
+		console.log(
+			'>> GALLERY CONFIG PRELIM',
+			'\n - galleryId:', galleryId,
+			'\n - type:', type,
+			'\n - categoryKey:', categoryKey,
+			'\n - filterMenu:', filterMenu,
+			'\n - galleryItems:', galleryItems,
+			''
+		);
+
+		// >>> Collect GR_thumbnail_item by .mix and Loop
+		let mixedUpGalleryItems = await galleryItems.each(async(i, item) => {
+			item = $(item);
+			console.log('>> GALLERY ITEM:', item);
 			
-			gallery = $(gallery);
-			// Collect GR_Id #category-<catKey>
-			let galleryId = gallery.attr('id');
-			// and split destructure for second key
-			let [type, categoryKey] = galleryId.split('-');
-			// find .gallery-filter-menu node
-			let filterMenu = gallery.find('.gallery-filter-menu');
-			let galleryContainer = gallery.find('.sort-collection');
-			let galleryItems = galleryContainer.children('.mix');
-			
-			// define fillter_collection
-			let galleryCollectedSubCategories = ['all'];
+			// Find <sub-cat-key> = .item-sub-category.text()
+			let subCategoryKey = await item.find('.item-sub-category').text();
+			// Create compound class via <catKey>-<sub-cat-key>
+			let compundClassAssignment = `${categoryKey}-${subCategoryKey}`;
+			// Add compound key as class to item
+			item.addClass(compundClassAssignment);
 
-			console.log(
-				'>> GALLERY CONFIG PRELIM',
-				'\n - galleryId:', galleryId,
-				'\n - type:', type,
-				'\n - categoryKey:', categoryKey,
-				'\n - asdf:', galleryContainer,
-				'\n - filterMenu:', filterMenu,
-				'\n - galleryItems:', galleryItems,
-				''
-			);
-
-			// >>> Collect GR_thumbnail_item by .mix and Loop
-			let mixedUpGalleryItems = await galleryItems.each(async(i, item) => {
-				item = $(item);
-				console.log('>> GALLERY ITEM:', item);
-				
-				// Find <sub-cat-key> = .item-sub-category.text()
-				let subCategoryKey = await item.find('.item-sub-category').text();
-				// Create compound class via <catKey>-<sub-cat-key>
-				let compundClassAssignment = `${categoryKey}-${subCategoryKey}`;
-				// Add compound key as class to item
-				item.addClass(compundClassAssignment);
-
-				// check collected gallery-filters and add
-				if (!galleryCollectedSubCategories.includes(subCategoryKey)) {
-					galleryCollectedSubCategories.push(subCategoryKey);
-				}
-			});
-			// <<< END 2ND LOOP
-
-			// >>> Loop on fillter_collection and generate filter menu
-			let mixedUpGallerFilterMenu = await galleryCollectedSubCategories.map((filterClass) => {
-				// Define attr key push
-				// Create compound class via <catKey>-<sub-cat-key>
-				let compundClassAssignment = `${categoryKey}-${filterClass}`;
-				let attributePush = (filterClass === "all")
-					? filterClass
-					: `.${compundClassAssignment}`;
-				// append filter menu link to .galler-filter-menu
-				filterMenu.append(
-					`<a href="#" class="filter w-button" data-filter="${attributePush}">${filterClass}</a>`
-				);
-			});
-			// <<< END 3RD LOOP
-
-			// Instantiate MixItUp ON GR_Id
-			return galleryContainer.mixItUp();
+			// check collected gallery-filters and add
+			if (!galleryCollectedSubCategories.includes(subCategoryKey)) {
+				galleryCollectedSubCategories.push(subCategoryKey);
+			}
 		});
-		// <<< END 1ST LOOP
+		// <<< END 2ND LOOP
+
+		// >>> Loop on fillter_collection and generate filter menu
+		let mixedUpGallerFilterMenu = await galleryCollectedSubCategories.map((filterClass) => {
+			// Define attr key push
+			// Create compound class via <catKey>-<sub-cat-key>
+			let compundClassAssignment = `${categoryKey}-${filterClass}`;
+			let attributePush = (filterClass === "all")
+				? filterClass
+				: `.${compundClassAssignment}`;
+			// append filter menu link to .galler-filter-menu
+			filterMenu.append(
+				`<a href="#" class="filter w-button" data-filter="${attributePush}">${filterClass}</a>`
+			);
+		});
+		// <<< END 3RD LOOP
+
+		// Instantiate MixItUp ON GR_Id
+		return galleryReel.mixItUp();
 
 		// End Of CONFIG SETUP >>
-
-		// // Find gallery targets
-		// let filterMenu = await $(".gallery-filter-menu");
-		// let sortCollection = await $(".sort-collection");
-		// let collectedSubCategoryFilters = ["all"];
-
-		// // @ each
-		// let collectedItems = await sortCollection
-		// 	.children(".mix")
-		// 	.each((i, item) => {
-		// 		// Find item-sub-category identifiers
-		// 		item = $(item);
-		// 		let classAssignment = item.find(".item-sub-category").text();
-		// 		console.log(">> CLASS ASS:", classAssignment);
-
-		// 		// and assign new sub cat class via the identified item-class text
-		// 		item.addClass(classAssignment);
-
-		// 		if (!collectedSubCategoryFilters.includes(classAssignment)) {
-		// 			// push to collected sub cat to filter list for later
-		// 			collectedSubCategoryFilters.push(classAssignment);
-		// 		}
-		// 	});
-		// console.log(">> FINISHED LOOP ON COLLECTED ITEMS:", collectedItems);
-
-		// // Generate filter menu
-		// console.log(">> GENERATE FILTER MENU ", collectedSubCategoryFilters);
-		// let filterMenuItems = await collectedSubCategoryFilters.map(
-		// 	subCatFilter => {
-		// 		console.log("> MAKE A FILTER BTN FOR:", subCatFilter);
-		// 		let attributePush =
-		// 			subCatFilter === "all" ? subCatFilter : `.${subCatFilter}`;
-		// 		filterMenu.append(
-		// 			`<a href="#" class="filter w-button" data-filter="${attributePush}">${subCatFilter}</a>`
-		// 		);
-		// 	}
-		// );
-
-		// // Clean Up
-		// $(".test-filter").remove();
-
-		// // Instantiate MixItUp:
-		// sortCollection.mixItUp();
 	});
 });
